24f673758d2e921bfadc679ba97b281e77afddcf
ADSmith-0 <adam00199382818@gmail.com>
2021-08-12 13:53:05 +0100

    update: added typescript and removed boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/App.module.css';
+import { useState } from 'react';
+function App() {
+  const [ playable, setPlayable ] = useState(false);
+  return (
+    <div>hello</div>
+  );
+}
+
+export default App;
